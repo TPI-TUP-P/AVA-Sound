e50215bdc06ed1cd32fedeeb4cd1cc58fc255f04
Agus-Sosa <sosaagustin351@gmail.com>
2026-04-22 13:52:27 -0300

    Add IRepository generic

--- src/Domain/Interfaces/IReviewRepository.cs
+++ src/Domain/Interfaces/IReviewRepository.cs
@@ -2,16 +2,10 @@ using Domain.Entities;
 
 namespace Domain.Interfaces;
 
-public interface IReviewRepository
+public interface IReviewRepository : IRepository<Review>
 {
-    Task<Review> GetById(Guid idReview);
 
     Task<List<Review>> GetBySong(Guid idSong);
 
-    Task Add(Review review);
-
-    Task Update(Review review);
-
-    Task Delete(Guid idReview);
 
 }
