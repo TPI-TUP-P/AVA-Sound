a421aacb0f63f99732a7db76e7ca9b68105a499c
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2026-04-29 14:35:47 -0300

    add repository no end

--- src/Infrastructure/Data/Repositories/ReviewRepository.cs
+++ src/Infrastructure/Data/Repositories/ReviewRepository.cs
@@ -3,3 +3,27 @@ namespace Infrastructure.Data.Repositories;
 using Domain.Entities;
 using Domain.Interfaces;
 
+public class ReviewRepository : IReviewRepository
+{
+    public Task<Review> GetBySong(Guid id)
+    {
+        return
+    }
+    public void GetById(Guid id)
+    {
+
+    }
+    public void Update()
+    {
+
+    }
+
+    public void Create()
+    {
+
+    }
+    public void Delete()
+    {
+
+    }
+}
