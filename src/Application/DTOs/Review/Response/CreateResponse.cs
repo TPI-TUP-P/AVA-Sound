8e0c752b378a716e7531dfa35aed2b64706ea8d5
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2026-05-03 20:19:07 -0300

    add abm review

--- src/Application/DTOs/Review/Response/CreateResponse.cs
+++ src/Application/DTOs/Review/Response/CreateResponse.cs
@@ -2,9 +2,9 @@ namespace Application.DTOs.Review.Response;
 
 public class CreateResponse
 {
-    public Guid IdUser { get; private set; }
+    public Guid IdUser { get; init; }
 
-    public Guid IdSong { get; private set; }
+    public Guid IdSong { get; init; }
 
     public string? Comment { get; set; }
     public DateTime DateCreated { get; set; }
--- src/Application/Interfaces/IReviewService.cs
+++ src/Application/Interfaces/IReviewService.cs
@@ -2,12 +2,12 @@ namespace Application.Interfaces;
 
 using Application.DTOs.Review.Request;
 using Application.DTOs.Review.Response;
-using Domain.Entities;
+
 
 public interface IReviewService
 {
     Task<List<GetBySongResponse>> GetBySong(Guid Id);
-    Task Create(CreateRequest reviewDto);
-    Task Update(Guid Id, UpdateRequest reviewDto);
+    Task<CreateResponse> Create(CreateRequest reviewDto);
+    Task<UpdateResponse> Update(Guid Id, UpdateRequest reviewDto);
     Task Delete(Guid id);
 }
