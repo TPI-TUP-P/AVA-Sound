98be15db3b6e41adce72f3266aa7435b48d0bc2f
andy <andyvolpe21@hotmail.com>
2026-05-07 14:27:01 -0300

    abm reproductionList-Song

---
+++ src/Application/DTOs/ReproductionList/Response/AddSongResponse.cs
@@ -0,0 +1,7 @@
+namespace Application.DTOs.ReproductionList.Response;
+
+public class AddSongResponse
+{
+    public Guid ReproductionListId { get; set; }
+        public Guid SongId { get; set; }
+}
---
+++ src/Application/Interfaces/ISongService.cs
@@ -0,0 +1,17 @@
+namespace Application.Interfaces;
+
+using Application.DTOs.Song.Response;
+using Application.DTOs.Song.Request;
+using Domain.Entities;
+
+
+public interface ISongService
+{
+    Task <GetByIdResponse> GetById(Guid id);
+    // Task <GetByAlbumResponse> GetByAlbum(Guid id);
+    // Task <GetByUserResponse> GetByUser(Guid id);
+    Task <List<GetAllResponse>> GetAll();
+    Task<CreateResponse> Create(CreateRequest songDto);
+    Task<UpdateResponse>  Update(Guid Id,UpdateRequest songDto);
+    Task Delete(Guid id);
+}
