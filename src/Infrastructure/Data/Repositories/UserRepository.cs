047301b9e9ab6bf564072b581d66bd6f77f6800b
andy <andyvolpe21@hotmail.com>
2026-05-13 10:10:55 -0300

    realizacion User+migracion

---
+++ src/Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,58 @@
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationContext _context;
+
+    public UserRepository(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User> GetById(Guid id)
+    {
+        var user= await _context.Users.FindAsync(id);
+        if (user==null)
+             throw new Exception($"El usuario con el ID {id} no fue encontrado.");
+
+        return user;
+    }
+
+
+    public async Task<List<User>> GetAll()
+{
+    return await _context.Users
+        .OrderByDescending(s => s.DateRegister)
+        .Take(30)
+        .ToListAsync();
+}
+
+    public async Task<User> Create(User user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<User> Update(User user)
+    {
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task Delete(Guid id)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        if (user != null)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
