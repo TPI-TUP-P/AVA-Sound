9d254905bf3883c00abcd77bf73752af688d8c20
andy <andyvolpe21@hotmail.com>
2026-05-17 19:15:30 -0300

    paginador user

--- src/Application/Interfaces/IUserService.cs
+++ src/Application/Interfaces/IUserService.cs
@@ -7,8 +7,9 @@ namespace Application.Interfaces;
 public interface IUserService
 {
     Task<GetByIdResponse> GetById(Guid id);
-    Task<List<GetAllResponse>> GetAll();
+    
     Task<CreateResponse> Create(CreateRequest UserDto, CancellationToken cancellationToken);
     Task<UpdateResponse> Update(Guid Id, UpdateRequest UsermDto);
     Task Delete(Guid id);
+    Task<PagerResponse<GetByIdResponse>> GetAll(PagerRequest pagerRequest);
 }
