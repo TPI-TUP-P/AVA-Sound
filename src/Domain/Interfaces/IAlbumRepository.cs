9179c7c0040d4053c82f261d40649ef1f2dc4c96
Agus-Sosa <sosaagustin351@gmail.com>
2026-05-21 17:13:51 -0300

    add new methods album

--- src/Domain/Interfaces/IAlbumRepository.cs
+++ src/Domain/Interfaces/IAlbumRepository.cs
@@ -6,6 +6,8 @@ namespace Domain.Interfaces;
 public interface IAlbumRepository : IRepository<Album>
 {
    Task<List<Album>> GetAll();
+
+   Task<List<Album>> GetAllByArtist(Guid idArtist, CancellationToken cancellationToken);
    Task<Album> AddSong(Guid id, Song song);
    
 }
